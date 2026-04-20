29b152ef5c15df7d2e8c5b316b0b35613eeb668b
shazaaly <shaza.aly@gmail.com>
2026-04-20 11:15:23 +0200

    adding user service

--- test-repo/src/violations/users.service.violations.ts
+++ test-repo/src/violations/users.service.violations.ts
@@ -6,7 +6,7 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class UsersService {
+export class UsersHandler {
   private readonly userRepo = {
     delete(targetId: string) {
       return { id: targetId, deleted: true };
